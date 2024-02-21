d30555e4fd35ab2565d5dd1de33fa98cc9591511
Huntshi <tribottetimeo@gmail.com>
2024-02-21 10:14:28 +0100

    Création de la classe Type

---
+++ PokemonProject/JSON/Type.js
@@ -0,0 +1,30 @@
+// Création de la class Type
+
+class Type {
+    all_types = ["Grass", "Poison", "Fire", "Flying", "Water", "Bug", "Normal", "Drak", "Electric", "Psychic", "Ice", "Steel", "Ground", "Fairy", "Fighting", "Rock", "Ghost", "Dragon"];
+
+    constructor(type_name, type_effectiveness) {
+        this.type_name = type_name;
+        this.type_effectiveness = type_effectiveness;
+    }
+
+    get getType_name() {
+        return this.type_name;
+    }
+
+    get getType_effectiveness() {
+        return this.type_effectiveness;
+    }
+
+    effectiveness() {
+        let tabEffectiveness = import('./type_effectiveness.js');
+        console.log(tabEffectiveness);
+    }
+
+    toString() {
+        return "Type : " + this.type_name + " Type d'efficacité : " + this.type_effectiveness;
+    }
+}
+
+let t1 = new Type("Poison", "Grass");
+console.log(t1.effectiveness());
