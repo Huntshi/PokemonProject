15739ae3b2325049d117b2cd3a47638476f22f77
mgourvest <milo.gourvest@etudiant.univ-rennes1.fr>
2024-03-14 11:33:26 +0100

    il ya index et les import

--- PokemonProject/JSON/pokemon.js
+++ PokemonProject/JSON/pokemon.js
@@ -1,16 +1,12 @@
-
 // Création de la class Pokemon
-
 class Pokemon {
-
-    constructor(base_attack, base_defense, base_stamina, form, pokemon_id, pokemon_name, type) {
+    constructor(base_attack, base_defense, base_stamina, pokemon_name, type) {
         this.base_attack = base_attack;
         this.base_defense = base_defense;
         this.base_stamina = base_stamina;
-        this.form = form;
-        this.pokemon_id = pokemon_id;
         this.pokemon_name = pokemon_name;
         this.type = type;
+        this.all_pokemons = [];
     }
 
     get getBase_attack() {
@@ -41,12 +37,38 @@ class Pokemon {
         return this.type;
     }
 
-
     toString() {
-        return "Nom : " + this.pokemon_name + " ID : " + this.pokemon_id + " Forme : " + this.form + " Attaque : " + this.base_attack + " Défense : " + this.base_defense + " Endurance : " + this.base_stamina + " Type : " + this.type;
+        return "Nom : " + this.pokemon_name +
+            " ID : " + this.pokemon_id +
+            " Forme : " + this.form +
+            " Attaque : " + this.base_attack +
+            " Défense : " + this.base_defense +
+            " Endurance : " + this.base_stamina +
+            " Type : " + this.type;
+    }
+
+    static importPokemon() {
+        let lstPokemon = [];
+
+        for (let i=0; i<pokemons.length; i++) {
+            if (pokemons[i].form == "Normal") {
+                // Partie pour récupérer le type du pokemon
+                let types = [];
+                for (let j=0; j<pokemon_types.length; j++) {
+                    if (pokemon_types[j].form == "Normal" && pokemon_types[i].pokemon_name === pokemons[i].pokemon_name) {
+                        types = pokemon_types[i].type;
+                        break;
+                    }
+                }
+
+                let poke = new Pokemon(pokemons[i].base_attack, pokemons[i].base_defense, pokemons[i].base_stamina, pokemons[i].pokemon_name, types);
+                lstPokemon.push(poke);
+            }
+        }
+
+        this.all_pokemons = lstPokemon;
     }
 }
 
-let p1 = new Pokemon(118, 111, 128, "Normal", 1, "Bulbizarre", "Plante");
-console.log(p1.toString());
-console.log(p1.getPokemon_id);
+console.log(Pokemon.importPokemon());
+console.log(Pokemon.all_pokemons[0]);
