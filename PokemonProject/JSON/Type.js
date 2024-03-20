72326386099dc09bbc96f0fa57fa676102bb6992
mgourvest <milo.gourvest@etudiant.univ-rennes1.fr>
2024-03-20 17:41:25 +0100

    on envoie le tout

--- PokemonProject/JSON/Type.js
+++ PokemonProject/JSON/Type.js
@@ -1,89 +1,36 @@
 // Création de la class Type
 class Type {
-
-    constructor() {
-        this.all_types = ["Grass", "Poison", "Fire", "Flying", "Water", "Bug", "Normal", "Drak", "Electric", "Psychic", "Ice", "Steel", "Ground", "Fairy", "Fighting", "Rock", "Ghost", "Dragon"];
-        this.value_effectiveness = 0;
-        this.type_name = [];
-        this.type_effectiveness_name = [];
-        this.pokemon_type = pokemon_types;
+    constructor(type_name, type_effectiveness_name) {
+        this.type_name = type_name;
+        this.type_effectiveness_name = type_effectiveness_name;
     }
-
-    /*
-
-    effectiveness(attack_type, defender_type) {
-        if (!this.all_types.includes(attack_type) || !this.all_types.includes(defender_type)) {
-            console.log("Les types d'attaque ou de défenseur ne sont pas valides !");
-            return null;
-        }
-        for (let i = 0; i < pokemon_types.length; i++) {
-            if (pokemon_types[i].form == "Normal") {
-
-                for (let k = 0; k < this.all_types.length; k++) {
-                    if (this.all_types[k] == defender_type) {
-
-                        switch (defender_type) {
-                            case 'Fire':
-                            case 'Flying':
-                            case 'Poison':
-                            case 'Steel':
-                            case 'Fighting':
-                            case 'Ghost':
-                            case 'Fairy':
-                                return this.value_effectiveness = 0.625;
-                                break;
-                            case 'Bug':
-                            case 'Dragon':
-                            case 'Electric':
-                            case 'Ground':
-                            case 'Ice':
-                            case 'Normal':
-                            case 'Rock':
-                            case 'Water':
-                                return this.value_effectiveness = 1.0;
-                                break;
-                            case 'Dark':
-                            case 'Psychic':
-                            case 'Grass':
-                                return this.value_effectiveness = 1.6;
-                                break;
-                            default:
-                                return print(`Type ${this.type_effectiveness[j]} n'existe pas`);
-                        }
-                    } else {
-                        console.log("Ce type n'existe pas");
-                    }
-                }
-            } else {
-                console.log("La forme de ce pokemon n'est pas normal");
-            }
-
-        }
-    }
-
-    */
 
     get getType_name() {
         return this.type_name;
+    }
+
+    set setType_name(newTypeName) {
+        this.type_name = newTypeName;
     }
 
     get getType_effectiveness_name() {
         return this.type_effectiveness_name;
     }
 
-    get getValue_effectiveness() {
-        return this.value_effectiveness;
-    }
-
-    get getTypes() {
-        return this.all_types;
+    set setType_effectiveness_name(newTypeEffectivenessName) {
+        this.type_effectiveness_name = newTypeEffectivenessName;
     }
 
     toString() {
-        return "Type : " + this.getType_name + ", Autre type: " + this.getType_effectiveness_name + ", Efficacité : " + this.getValue_effectiveness;
+        let effectivenessString = "{";
+        for (const [type, effectiveness] of Object.entries(this.type_effectiveness_name)) {
+            effectivenessString += type + ": " + effectiveness + ", ";
+        }
+        // Suppression de la virgule et de l'espace en trop à la fin
+        effectivenessString = effectivenessString.slice(0, -2) + "}";
+        
+        return "\n\nNom du type : " + this.type_name +
+            "\nEfficacité du type : " + effectivenessString;
     }
+    
 }
-
-/*let t1 = new Type("Poison", "Grass");
-const t1 = new Type();
-console.log(t1.getTypes);*/
