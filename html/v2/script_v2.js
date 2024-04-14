b01c785c72f09fc5f1fd7e7f3063afe783698063
Huntshi <tribottetimeo@gmail.com>
2024-04-14 19:27:18 +0200

    css

--- html/v2/script_v2.js
+++ html/v2/script_v2.js
@@ -4,6 +4,28 @@ const pageSize = 25;
 // Récupérer la page actuelle depuis le stockage local
 let currentPage = localStorage.getItem('currentPage') || 1;
 
+// Tableau des couleurs des types et des icones respective
+const typeColorsAndIcons = {
+    "Normal": { color: "#A8A77A", icon: "../webp/images/08072021-normal-removebg-preview.png" },
+    "Fire": { color: "#EE8130", icon: "../webp/images/08072021-feu-removebg-preview.png" },
+    "Water": { color: "#6390F0", icon: "../webp/images/08072021-eau-removebg-preview.png" },
+    "Electric": { color: "#F7D02C", icon: "../webp/images/08072021-electrik-removebg-preview.png" },
+    "Grass": { color: "#7AC74C", icon: "../webp/images/08072021-plante-removebg-preview.png" },
+    "Ice": { color: "#96D9D6", icon: "../webp/images/08072021-glace-removebg-preview.png" },
+    "Fighting": { color: "#C22E28", icon: "../webp/images/08072021-combat-removebg-preview.png" },
+    "Poison": { color: "#A33EA1", icon: "../webp/images/08072021-poison-removebg-preview.png" },
+    "Ground": { color: "#E2BF65", icon: "../webp/images/08072021-sol-removebg-preview.png" },
+    "Flying": { color: "#A98FF3", icon: "../webp/images/08072021-vol-removebg-preview.png" },
+    "Psychic": { color: "#F95587", icon: "../webp/images/08072021-psy-removebg-preview.png" },
+    "Bug": { color: "#A6B91A", icon: "../webp/images/08072021-insecte-removebg-preview.png" },
+    "Rock": { color: "#B6A136", icon: "../webp/images/08072021-roche-removebg-preview.png" },
+    "Ghost": { color: "#735797", icon: "../webp/images/08072021-spectre-removebg-preview.png" },
+    "Dragon": { color: "#6F35FC", icon: "../webp/images/08072021-dragon-removebg-preview.png" },
+    "Dark": { color: "#705746", icon: "../webp/images/08072021-tnbre-removebg-preview.png" },
+    "Steel": { color: "#B7B7CE", icon: "../webp/images/08072021-acier-removebg-preview.png" },
+    "Fairy": { color: "#D685AD", icon: "../webp/images/08072021-fe-removebg-preview.png" },
+  };
+
 function transformInt(id) {
     let stringId = id.toString();
 
@@ -35,13 +57,69 @@ function renderPokemonPage(pageNumber) {
         const defenseCase = document.createElement('td');
         const imageCase = document.createElement('td');
 
+        // Ajout d'un gestionnaire d'événements pour chaque ligne
+        ligne.addEventListener('click', function() {
+            displayPokemonContextMenu(event, pokemon);
+        });
+
         idCase.textContent = pokemon.id;
         nameCase.textContent = pokemon.pokemon_name;
         generationCase.textContent = pokemon.generation;
-        typesCase.textContent = pokemon.type;
+        //typesCase.textContent = pokemon.type;
         enduranceCase.textContent = pokemon.base_stamina;
         attaqueCase.textContent = pokemon.base_attack;
         defenseCase.textContent = pokemon.base_defense;
+
+        const typesDiv = document.createElement("div");
+        typesDiv.className = "pokemon-types";
+
+        pokemon.type.forEach((type) => {
+        const typeTrimmed = type.trim();
+    
+        // Vérifiez si l'objet correspondant au type de Pokémon existe dans le tableau typeColorsAndIcons
+        if (typeColorsAndIcons.hasOwnProperty(typeTrimmed)) {
+        const typeIcon = document.createElement("img");
+        typeIcon.src = typeColorsAndIcons[typeTrimmed].icon;
+        typeIcon.width = 20;
+        typeIcon.height = 20;
+        typeIcon.alt = typeTrimmed + " Icon";
+    
+        const typeName = document.createElement("span");
+        typeName.textContent = typeTrimmed;
+        typeName.style.color = typeColorsAndIcons[typeTrimmed].color;
+    
+        typesDiv.appendChild(typeIcon);
+        typesDiv.appendChild(typeName);
+        } else {
+        console.error("Erreur: le type de Pokémon '" + typeTrimmed + "' n'est pas trouvé dans typeColorsAndIcons.");
+        }
+    });
+
+        typesCase.appendChild(typesDiv);
+
+        const enduranceIcon = document.createElement('img');
+        enduranceIcon.src = '../webp/images/donut-chart-fill.svg'; // Remplacez par le chemin réel de l'icône d'endurance
+        enduranceIcon.width = 20;
+        enduranceIcon.height = 20;
+        enduranceIcon.alt = 'Endurance Icon';
+        enduranceCase.textContent = pokemon.base_stamina;
+        enduranceCase.appendChild(enduranceIcon);
+
+        const attackIcon = document.createElement('img');
+        attackIcon.src = '../webp/images/sword-fill.svg'; // Remplacez par le chemin réel de l'icône d'attaque
+        attackIcon.width = 20;
+        attackIcon.height = 20;
+        attackIcon.alt = 'Attack Icon';
+        attaqueCase.textContent = pokemon.base_attack;
+        attaqueCase.appendChild(attackIcon);
+
+        const defenseIcon = document.createElement('img');
+        defenseIcon.src = '../webp/images/shield-fill.svg'; // Remplacez par le chemin réel de l'icône de défense
+        defenseIcon.width = 20;
+        defenseIcon.height = 20;
+        defenseIcon.alt = 'Defense Icon';
+        defenseCase.textContent = pokemon.base_defense;
+        defenseCase.appendChild(defenseIcon);
 
         const image = document.createElement('img');
         let imageId = transformInt(pokemon.id);
